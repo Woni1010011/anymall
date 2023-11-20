341d611941bd6d9c9f816f900e1c8f733390d1da
woni1010011 <romoney@naver.com>
2023-11-20 13:54:29 +0900

    Feat: StartProject
    settings.py 암호화,
    secrets.json 파일 추가,
    requirements.txt 추가,
    gitignore 추가
    
    작성자: 박성원

---
+++ anymall/anymall/readkey.py
@@ -0,0 +1,17 @@
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
