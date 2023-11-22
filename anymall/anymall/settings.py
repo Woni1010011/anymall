a2d6815d6f062df9f40e8e07787c2f6532e56d05
woni1010011 <romoney@naver.com>
2023-11-22 10:23:39 +0900

    Feat: admin 앱 추가
    anymall_admin 앱 추가
    
    작성자: 박성원

--- anymall/anymall/settings.py
+++ anymall/anymall/settings.py
@@ -35,7 +35,6 @@ def get_secret(setting, secrets=secrets):
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
-print(get_secret("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
