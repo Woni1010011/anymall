67fc1e79a9b52611cd8e17c7ffe2adb8e9a673e9
woni1010011 <romoney@naver.com>
2023-11-23 00:07:03 +0900

    Feat: models.py
    유저테이블, 등급 테이블 생성
    
    작성자: 박성원

--- anymall/main/models.py
+++ anymall/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Grade(models.Model):
+    grade = models.CharField(max_length=20, unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.grade
+class User(models.Model):
+    user_no = models.AutoField(primary_key=True)
+    user_email = models.EmailField(unique=True)
+    user_password = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=50)
+    user_phone = models.CharField(max_length=20, null=True, blank=True)
+    zip_code = models.CharField(max_length=10, null=True, blank=True)
+    user_address = models.TextField(null=True, blank=True)
+    user_point = models.IntegerField(default=0)
+    is_email = models.BooleanField(default=True)
+    sub_date = models.DateField(auto_now_add=True)
+    grade = models.ForeignKey(Grade, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user_name
+    
