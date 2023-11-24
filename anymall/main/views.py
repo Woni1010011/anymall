ad9629296ed85b80d620dd4600d2a60b73675a18
woni1010011 <romoney@naver.com>
2023-11-24 17:20:54 +0900

    Test: mypage User정보 불러오기
    migrate 후 User 생성 후 정보를 불러옴
    
    작성자: 박성원

--- anymall/main/views.py
+++ anymall/main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import User
 
 def home(request):
+    user = User.objects.get()
     return render(request, "index.html")
 
 
@@ -10,13 +11,20 @@ def shop(request):
 
 
 def mypage(request):
-    if request.session.get("user_email"):
-        user_email = request.session["user_email"]
+    # if request.session.get("user_email"):
+    #     user_email = request.session["user_email"]
 
-        try:
-            user = User.objects.get(user_email=user_email)
-            return render(request, "mypage.html", user)
-        except User.DoesNotExist:
-            return redirect("home")
+    #     try:
+    #         user = User.objects.get(user_email=user_email)
+    #         return render(request, "mypage.html", user)
+    #     except User.DoesNotExist:
+    #         return redirect("home")
 
-    return render(request, "mypage.html")
+    user = get_object_or_404(User, user_no=8)
+
+
+    context = {
+        "user_name" : user.user_name
+    }
+
+    return render(request, "mypage.html", context)
