1a17c5ed188f1a805ae68c9e1b2d2c4006b83c6b
chorongfire33 <choyeon8317@naver.com>
2023-11-24 11:47:11 +0900

    Feat: models.py Administrator / OptionList, Stock

--- anymall/main/models.py
+++ anymall/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class OptionList(models.Model):
+    option_no = models.AutoField(primary_key=True)
+    product_no = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="options"
+    )
+    option_name = models.CharField(max_length=20, null=False)
+    option_value = models.CharField(max_length=20, null=False)
+    option_amount = models.IntegerField(default=0, null=False)
+
+    def __str__(self):
+        return self.option_name
+
+
+class Stock(models.Model):
+    product_no = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="options"
+    )
+    category_id = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="categories"
+    )
+    # category_id Product reference 아닐 경우 Category 로 edit
