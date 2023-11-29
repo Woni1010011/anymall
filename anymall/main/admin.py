deb95e433b91166fd6819e72e27ea8e249cc69ba
chorongfire33 <choyeon8317@naver.com>
2023-11-29 15:35:08 +0900

    Feat: models.py write CustomUser

--- anymall/main/admin.py
+++ anymall/main/admin.py
@@ -1,23 +1,48 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser
+
 
 # CustomUser 모델을 관리자 사이트에 등록하고 CustomUserAdmin 클래스로 관리
 @admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     # 관리자 페이지에서 사용자 목록에 표시될 필드들
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ("username", "email", "is_staff")
+    list_display_links = (
+        "username",
+        "email",
+    )
+    list_filter = (
+        "username",
+        "email",
+    )
+    search_fields = (
+        "username",
+        "email",
+    )
 
     # 관리자 페이지에서 사용자 상세 페이지를 어떻게 구성할지 결정
     fieldsets = (
-        ('User Credentials', {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                   'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        ('Custom fields', {'fields': ('user_phone', 'zip_code', 'user_address', 'user_point', 'user_type', 'sub_date', 'grade')}),
+        ("User Credentials", {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_admin",)}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+        (
+            "Custom fields",
+            {
+                "fields": (
+                    "user_phone",
+                    "zip_code",
+                    "user_address",
+                    "user_point",
+                    "user_type",
+                    "sub_date",
+                    "grade",
+                )
+            },
+        ),
     )
 
     # 검색 기능에 사용될 필드들
-    search_fields = ('email', 'first_name', 'last_name', 'user_phone')
-    ordering = ('email',)
+    search_fields = ("email", "first_name", "last_name", "user_phone")
+    ordering = ("email",)
--- anymall/main/models.py
+++ anymall/main/models.py
@@ -1,51 +1,100 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 
 
 # Create your models here.
 
+from django.contrib.auth.models import BaseUserManager  # 임포트
 
 
+# custom user model 사용 시 UserManager 클래스와 create_user, create_superuser 함수가 정의되어 있어야 함
+class UserManager(BaseUserManager):
+    # 필수로 필요한 데이터를 선언
+    def create_user(self, email, username, password=None):
+        if not username:
+            raise ValueError("Users must have an username")
+        user = self.model(
+            email=email,
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    # category_id Product reference 아닐 경우 Category 로 edit
-class Grade(models.Model):
-    grade_id = models.AutoField(primary_key=True)
-    grade = models.CharField(max_length=20, null=True, blank=True)
-    discount = models.IntegerField(null=True, blank=True)
+    # python manage.py createsuperuser 사용 시 해당 함수가 사용됨
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(email=email, username=username, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
-    def __str__(self):
-        return f"{self.grade} - Discount: {self.discount}%"
-    
-class CustomUser(AbstractUser):
+
+class CustomUser(AbstractBaseUser):
     # AbstractUser에서 username, email, first_name, last_name 등의 필드를 이미 제공하므로 중복되지 않게 주의
     user_no = models.BigAutoField(primary_key=True)
-    # email 필드는 AbstractUser에서 제공하므로 추가할 필요 없음
+    username = models.CharField("사용자 이름", max_length=20, unique=True)
+    password = models.CharField("비밀번호", max_length=128)  # 해시되기 때문에 max_length가 길어야함
+    email = models.EmailField("사용자 계정", max_length=100, unique=True)
     user_phone = models.CharField(max_length=11, unique=True)
     zip_code = models.IntegerField(null=True, blank=True)
     user_address = models.CharField(max_length=200, null=True, blank=True)
     user_point = models.IntegerField(default=0)
     email_check = models.CharField(max_length=5, null=True, blank=True)
-    user_type = models.IntegerField(null=True, blank=True)
+    user_type = models.IntegerField(default=0, blank=True)
     sub_date = models.DateTimeField(auto_now_add=True)
-    # Grade 모델을 참조하는 외래 키
-    grade = models.ForeignKey('Grade', on_delete=models.CASCADE)
-    
-class Category(models.Model) :
+    grade = models.CharField(default="Bronze", max_length=20, null=False)
+
+    # 관리자 권한 여부 (기본값은 False)
+    is_admin = models.BooleanField(default=False)
+
+    # 실제 로그인에 사용되는 아이디
+    USERNAME_FIELD = "email"
+
+    # 어드민 계정을 만들 때 입력받을 정보 ex) email
+    # 사용하지 않더라도 선언이 되어야함
+    # USERNAME_FIELD와 비밀번호는 기본적으로 포함되어있음
+    REQUIRED_FIELDS = ["username"]
+    # custom user 생성 시 필요
+    objects = UserManager()
+
+    # # 어드민 페이지에서 데이터를 제목을 어떻게 붙여줄 것인지 지정
+    # def __Str__(self):
+    #     return f"{self.username} / {self.email} 님의 계정입니다"
+
+    # 로그인 사용자의 특정 테이블의 crud 권한을 설정, perm table의 crud 권한이 들어간다.
+    # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
+    # 일반적으로 선언만 해두고 건들지않는다
+    def has_perm(self, perm, obj=None):
+        return True
+
+    # 로그인 사용자의 특정 app에 접근 가능 여부를 설정, app_label에는 app 이름이 들어간다.
+    def has_module_perms(self, app_label):
+        return True
+
+    # admin 권한 설정
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
+class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     category_name = models.CharField(max_length=20)
     pass
 
-    
-class Product(models.Model) :
+
+class Product(models.Model):
     product_no = models.AutoField(primary_key=True)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=50)
     product_price = models.IntegerField(default=0)
     is_option = models.BooleanField(default=True)
     product_date = models.DateField(auto_now_add=True)
-    product_thumnail = models.ImageField(upload_to='product_thumnails/')
+    product_thumnail = models.ImageField(upload_to="product_thumnails/")
     product_description = models.TextField(1000)
-    product_images = models.ManyToManyField('ProductImage', related_name='product_images')
+    product_images = models.ManyToManyField(
+        "ProductImage", related_name="product_images"
+    )
     is_display = models.BooleanField(default=True)
     is_for_sale = models.BooleanField(default=True)
     sales_volume = models.IntegerField(default=0)
@@ -61,12 +110,14 @@ class Stock(models.Model):
         Product, on_delete=models.CASCADE, related_name="categories"
     )
 
-class ProductImage(models.Model) :
-    image = models.ImageField(upload_to='product_images/')
 
-    def __str__(self) :
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to="product_images/")
+
+    def __str__(self):
         return f"Image {self.id}"
-    
+
+
 class OptionList(models.Model):
     option_no = models.AutoField(primary_key=True)
     product_no = models.ForeignKey(
