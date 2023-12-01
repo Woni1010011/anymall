c37344b3ef731ab327a80707114ee532de0402f7
AGG-J <hyojooagnes824@gmail.com>
2023-12-01 14:55:40 +0900

    Feat: OptionList
    상품등록 시 옵션등록기능 추가
    
    작성자: 정효주

--- anymall/main/forms.py
+++ anymall/main/forms.py
@@ -6,6 +6,17 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['category_id', 'product_name', 'product_price', 'is_option', 'product_thumnail', 'product_description', 'is_display', 'is_for_sale', 'sales_volume']
         
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['sales_volume'].required = False
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['sales_volume'].required = False
+    
+    # Add option_name, option_value, and option_amount fields
+    option_name = forms.CharField(max_length=20, required=False)
+    option_value = forms.CharField(max_length=20, required=False)
+    option_amount = forms.IntegerField(required=False)
+
+    # Add prefix to option fields to handle multiple options
+    def add_prefix(self, field_name):
+        if field_name in ['option_name', 'option_value', 'option_amount']:
+            return f'options-{field_name}'
+        return super().add_prefix(field_name)
